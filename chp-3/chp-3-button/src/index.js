b10acbf45a1049bf0d1b7470076d7bd1ec99e80a
Paul Piper <paul.piper@codecentric.de>
2018-03-09 13:23:56 +0100

    added task for chp-3: add a button

--- chp-3/chp-3-button/src/index.js
+++ chp-3/chp-3-button/src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM, {render} from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import Button from './components/Button';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('main'));
 registerServiceWorker();
+
+render(<Button/>, document.querySelector('#main'));
