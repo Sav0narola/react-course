a47b047214c34ed9d3082d5dcc5caaa1383af72e
Paul Piper <paul.piper@codecentric.de>
2018-03-12 11:27:20 +0100

    cleaned up master branch, implemented course starter files

--- chp-3/chp-3-button/src/index.js
+++ chp-3/chp-3-button/src/index.js
@@ -3,9 +3,6 @@ import ReactDOM, {render} from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import Button from './components/Button';
 
 ReactDOM.render(<App />, document.getElementById('main'));
 registerServiceWorker();
-
-render(<Button/>, document.querySelector('#main'));
