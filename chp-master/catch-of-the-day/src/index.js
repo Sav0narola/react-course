7c11c5ff6aafa54b12d378c4ab2d0657545f95dc
Paul Piper <paul.piper@codecentric.de>
2018-03-13 16:29:14 +0100

    implemented files for chp-4, chp-5 and chp-6.

--- chp-master/catch-of-the-day/src/index.js
+++ chp-master/catch-of-the-day/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import StorePicker from './components/StorePicker.js';
+import './css/style.css';
+import App from "./components/App";
 
-render(<StorePicker />, document.querySelector('#main'));
+render(<App />, document.querySelector('#main'));
